ecc2c2bc18a696e5d120d5419744b5a7c94bab04
Flowseal <trololo424@yandex.ru>
2023-10-12 21:47:02 +0300

    Едитор для активного слайда

---
+++ src/components/SlideEditor.tsx
@@ -0,0 +1,11 @@
+import styles from './SlideEditor.module.css'
+
+const SlideEditor = () => {
+  return (
+    <div className={styles.slideEditor}>
+        
+    </div>
+  )
+}
+
+export default SlideEditor
