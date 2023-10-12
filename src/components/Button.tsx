a3589f68a4e9db1a68b93ef60dcd0964cc863aa6
Flowseal <trololo424@yandex.ru>
2023-10-13 02:22:46 +0300

    Новая кнопочка

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,23 @@
 import styles from './Button.module.css'
 
 type ButtonProps = {
-  label: string
-  onClick: () => void
+  width?: string;
+  height?: string;
+  children: React.ReactNode;
 }
 
 const Button = (props: ButtonProps) => {
+  const width = props.width || "auto";
+  const height = props.height || "auto";
+
+  const buttonSizes = {
+    width: width,
+    height: height,
+  };
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
-      <div>{props.label}</div>
+    <button className={styles.button} style={buttonSizes}>
+      {props.children}
     </button>
   )
 }
