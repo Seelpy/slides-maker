1a9333416c4785cbc70355d02e4993c1056cc977
Flowseal <trololo424@yandex.ru>
2023-11-15 10:42:40 +0300

    Фикс бага с изменением активного слайда

--- src/components/Slide/SlideEditor.tsx
+++ src/components/Slide/SlideEditor.tsx
@@ -1,20 +1,24 @@
 import styles from './SlideEditor.module.css'
 import EditorObject from './SlideObject';
-import { SlideInfo } from '../../models/types';
+import { useAppSelector } from '../../hooks/redux';
 import { useInterfaceActions } from '../../hooks/redux';
 
-type SlideEditorProps = {
-  slideInfo: SlideInfo
-};
-
-const SlideEditor = (props: SlideEditorProps) => {
+const SlideEditor = () => {
+  const activeSlide = useAppSelector(state => state.interfaceReducer.activeSlide);
+  const slides = useAppSelector(state => state.presentationReducer.slides);
   const {setDragObjects} = useInterfaceActions();
+
+  if (activeSlide === undefined) {
+    return (null);
+  }
+
+  const referencedActiveSlide = slides.find(s => s.id === activeSlide.id);
 
   return (
     <div className={styles.editorArea} onMouseLeave={() => setDragObjects(false)}>
       <div className={styles.slideEditor}>
-          {props.slideInfo.slide.map((obj, i) => (
-            <EditorObject key={i} slide={props.slideInfo} data={obj} preview={false}/>
+          {referencedActiveSlide!.slide.map((obj, i) => (
+            <EditorObject key={i} slide={referencedActiveSlide!} data={obj} preview={false}/>
           ))}
       </div>
     </div>
