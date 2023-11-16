9d5ac4ffaa38d2a1f4951cf4f92b7154b44bcfa9
Flowseal <trololo424@yandex.ru>
2023-11-16 11:06:08 +0300

    Создание слайда

--- src/components/UI/MenuSections/index.ts
+++ src/components/UI/MenuSections/index.ts
@@ -3,5 +3,6 @@ import SectionFigures from "./SectionFigures";
 import SectionImages from "./SectionImages";
 import SectionInfo from "./SectionInfo";
 import SectionText from "./SectionText";
+import SectionSlides from "./SectionSlides";
 
-export {SectionColors, SectionFigures, SectionImages, SectionInfo, SectionText}
+export {SectionColors, SectionFigures, SectionImages, SectionInfo, SectionText, SectionSlides}
