b854674d7417bbd15f8ffb76536693d9d45d0636
Flowseal <trololo424@yandex.ru>
2023-12-20 01:46:59 +0300

    Теперь хук с хоткеями может принимать несколько хоткеев сразу. Исправил баг с кириллицей

--- src/hooks/useHotkey.ts
+++ src/hooks/useHotkey.ts
@@ -1,10 +1,10 @@
 import { useEffect } from "react"
 
-function useHotkey(data: {hotkey: string, callback: (event: KeyboardEvent) => void}) {
+function useHotkey(hotkeys: string[], callback: (event: KeyboardEvent) => void) {
   // Разделяет строку по первому '+' после конца слова
-  const hotkeyArray = Array.from(new Set(
-    data.hotkey.toLocaleLowerCase().replace(/\s+/g, "").split(/\b\+/g)
-  ))
+  const hotkeyArrays = hotkeys.map((hotkey) => Array.from(new Set(
+    hotkey.toLocaleLowerCase().replace(/\s+/g, "").split(/\b\+/g)
+  )))
 
   const getActiveKeysArray = (event: KeyboardEvent) => {
     const activeKeys: Set<string> = new Set()
@@ -17,17 +17,27 @@ function useHotkey(data: {hotkey: string, callback: (event: KeyboardEvent) => vo
 
   const isSameHotkeys = (event: KeyboardEvent): boolean => {
     const activeKeys = getActiveKeysArray(event)
-    if (hotkeyArray.length === 1 && hotkeyArray[0] === "*") {
-      return true
+    
+    for (let i = 0; i < hotkeyArrays.length; i++) {
+      // Случай с символом '*' - любая клавиша вызывает коллбек
+      if (hotkeyArrays[i].length === 1 && hotkeyArrays[i][0] === "*") {
+        return true
+      }
+
+      // Если массив клавиш одинаков с массивом активных клавиш
+      if (hotkeyArrays[i].length === activeKeys.length &&
+          hotkeyArrays[i].every((key) => activeKeys.includes(key))) {
+        return true
+      }
     }
-    return hotkeyArray.length === activeKeys.length && 
-      hotkeyArray.every((key) => activeKeys.includes(key))
+    
+    return false
   }
 
   useEffect(() => {
     const onKeyDown = (event: KeyboardEvent) => {
       if (isSameHotkeys(event)) {
-        data.callback(event)
+        callback(event)
       }
     }
 
@@ -36,7 +46,7 @@ function useHotkey(data: {hotkey: string, callback: (event: KeyboardEvent) => vo
     return () => {
       document.removeEventListener("keydown", onKeyDown)
     }
-  }, [data])
+  }, [hotkeys, callback])
 }
 
 export default useHotkey 
